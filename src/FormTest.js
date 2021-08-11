8666b50cc22ba8756a10f2a783c50842b3872c1a
TakashiNishimura <57384281+TakashiNishimura@users.noreply.github.com>
2021-08-12 00:08:28 +0900

    api call2

--- src/FormTest.js
+++ src/FormTest.js
@@ -25,27 +25,25 @@ export class FormTest extends Component {
       value: '',
       message: value
     });
-    var callAPI = (firstName,lastName)=>{
-      // instantiate a headers object
-      var myHeaders = new Headers();
-      // add content type header to object
-      myHeaders.append("Content-Type", "application/json");
-      // using built in JSON utility package turn object to string and store in a variable
-      var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
-      // create a JSON object with parameters for API call and store in a variable
-      var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-      // make API call with parameters and use promises to get response
-      fetch("https://viwy3zwne1.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
-        .then(response => response.text())
-        .then(result => alert(JSON.parse(result).body))
-        .catch(error => console.log('error', error));
-    }
-    callAPI("","")
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"firstName":"a","lastName":"b"});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://viwy3zwne1.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .then(result => alert(JSON.parse(result).body))
+      .catch(error => console.log('error', error));
   }
 
   render() {
