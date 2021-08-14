d3f7ab3ca8d574eb307eac067d44522edf1ec0e3
TakashiNishimura <57384281+TakashiNishimura@users.noreply.github.com>
2021-08-14 14:59:33 +0900

    k

--- src/APITest.js
+++ src/APITest.js
@@ -3,140 +3,140 @@ import React, {useState, useEffect} from "react";
 const APITest = () => {
 
   const [posts, setPosts] = useState({
-    publicTime: "",
-    publicTimeFormatted: "",
-    publishingOffice: "",
-    title: "",
-    link: "",
-    description: {
-      publicTime: "",
-      publicTimeFormatted: "",
-      headlineText: "",
-      bodyText: "",
-      text: ""
-    },
-    forecasts: [
-      {
-        date: "",
-        dateLabel: "",
-        telop: "",
-        detail: {
-          weather: "",
-          wind: "",
-          wave: ""
-        },
-        temperature: {
-          min: {
-            celsius: "",
-            fahrenheit: ""
-          },
-          max: {
-            celsius: "",
-            fahrenheit: ""
-          }
-        },
-        chanceOfRain: {
-          T00_06: "",
-          T06_12: "",
-          T12_18: "",
-          T18_24: ""
-        },
-        image: {
-          title: "",
-          url: "",
-          width: 0,
-          height: 0
-        }
-      },
-      {
-        date: "",
-        dateLabel: "",
-        telop: "",
-        detail: {
-          weather: "",
-          wind: "",
-          wave: ""
-        },
-        temperature: {
-          min: {
-            celsius: "",
-            fahrenheit: ""
-          },
-          max: {
-            celsius: "",
-            fahrenheit: ""
-          }
-        },
-        chanceOfRain: {
-          T00_06: "",
-          T06_12: "",
-          T12_18: "",
-          T18_24: ""
-        },
-        image: {
-          title: "",
-          url: "",
-          width: 0,
-          height: 0
-        }
-      },
-      {
-        date: "",
-        dateLabel: "",
-        telop: "",
-        detail: {
-          weather: "",
-          wind: "",
-          wave: ""
-        },
-        temperature: {
-          min: {
-            celsius: "",
-            fahrenheit: ""
-          },
-          max: {
-            celsius: "",
-            fahrenheit: ""
-          }
-        },
-        chanceOfRain: {
-          T00_06: "",
-          T06_12: "",
-          T12_18: "",
-          T18_24: ""
-        },
-        image: {
-          title: "",
-          url: "",
-          width: 0,
-          height: 0
-        }
-      }
-    ],
-    location: {
-      area: "",
-      prefecture: "",
-      district: "",
-      city: ""
-    },
-    copyright: {
-      title: "",
-      link: "",
-      image: {
-        title: "",
-        link: "",
-        url: "",
-        width: 0,
-        height: 0
-      },
-      provider: [
-        {
-          link: "",
-          name: "",
-          note: ""
-        }
-      ]
-    }
+    // publicTime: "",
+    // publicTimeFormatted: "",
+    // publishingOffice: "",
+    // title: "",
+    // link: "",
+    // description: {
+    //   publicTime: "",
+    //   publicTimeFormatted: "",
+    //   headlineText: "",
+    //   bodyText: "",
+    //   text: ""
+    // },
+    // forecasts: [
+    //   {
+    //     date: "",
+    //     dateLabel: "",
+    //     telop: "",
+    //     detail: {
+    //       weather: "",
+    //       wind: "",
+    //       wave: ""
+    //     },
+    //     temperature: {
+    //       min: {
+    //         celsius: "",
+    //         fahrenheit: ""
+    //       },
+    //       max: {
+    //         celsius: "",
+    //         fahrenheit: ""
+    //       }
+    //     },
+    //     chanceOfRain: {
+    //       T00_06: "",
+    //       T06_12: "",
+    //       T12_18: "",
+    //       T18_24: ""
+    //     },
+    //     image: {
+    //       title: "",
+    //       url: "",
+    //       width: 0,
+    //       height: 0
+    //     }
+    //   },
+    //   {
+    //     date: "",
+    //     dateLabel: "",
+    //     telop: "",
+    //     detail: {
+    //       weather: "",
+    //       wind: "",
+    //       wave: ""
+    //     },
+    //     temperature: {
+    //       min: {
+    //         celsius: "",
+    //         fahrenheit: ""
+    //       },
+    //       max: {
+    //         celsius: "",
+    //         fahrenheit: ""
+    //       }
+    //     },
+    //     chanceOfRain: {
+    //       T00_06: "",
+    //       T06_12: "",
+    //       T12_18: "",
+    //       T18_24: ""
+    //     },
+    //     image: {
+    //       title: "",
+    //       url: "",
+    //       width: 0,
+    //       height: 0
+    //     }
+    //   },
+    //   {
+    //     date: "",
+    //     dateLabel: "",
+    //     telop: "",
+    //     detail: {
+    //       weather: "",
+    //       wind: "",
+    //       wave: ""
+    //     },
+    //     temperature: {
+    //       min: {
+    //         celsius: "",
+    //         fahrenheit: ""
+    //       },
+    //       max: {
+    //         celsius: "",
+    //         fahrenheit: ""
+    //       }
+    //     },
+    //     chanceOfRain: {
+    //       T00_06: "",
+    //       T06_12: "",
+    //       T12_18: "",
+    //       T18_24: ""
+    //     },
+    //     image: {
+    //       title: "",
+    //       url: "",
+    //       width: 0,
+    //       height: 0
+    //     }
+    //   }
+    // ],
+    // location: {
+    //   area: "",
+    //   prefecture: "",
+    //   district: "",
+    //   city: ""
+    // },
+    // copyright: {
+    //   title: "",
+    //   link: "",
+    //   image: {
+    //     title: "",
+    //     link: "",
+    //     url: "",
+    //     width: 0,
+    //     height: 0
+    //   },
+    //   provider: [
+    //     {
+    //       link: "",
+    //       name: "",
+    //       note: ""
+    //     }
+    //   ]
+    // }
   });
 
   useEffect(() => {
