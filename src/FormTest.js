37427216753859747cf28d29e367d765a6be23fb
TakashiNishimura <57384281+TakashiNishimura@users.noreply.github.com>
2021-08-14 17:59:20 +0900

    x

--- src/FormTest.js
+++ src/FormTest.js
@@ -6,51 +6,169 @@ export class FormTest extends Component {
     super(props);
 
     this.state = {
-      value: '',
-      message: ''
+      publicTime: "",
+      publicTimeFormatted: "",
+      publishingOffice: "",
+      title: "",
+      link: "",
+      description: {
+        publicTime: "",
+        publicTimeFormatted: "",
+        headlineText: "",
+        bodyText: "",
+        text: ""
+      },
+      forecasts: [
+        {
+          date: "",
+      dateLabel: "",
+      telop: "",
+      detail: {
+        weather: "",
+        wind: "",
+        wave: ""
+      },
+      temperature: {
+        min: {
+          celsius: "",
+          fahrenheit: ""
+        },
+        max: {
+          celsius: "",
+          fahrenheit: ""
+        }
+      },
+      chanceOfRain: {
+        T00_06: "",
+        T06_12: "",
+        T12_18: "",
+        T18_24: ""
+      },
+      image: {
+        title: "",
+        url: "",
+        width: 0,
+        height: 0
+      }
+    },
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      },
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      }
+  ],
+    location: {
+      area: "",
+        prefecture: "",
+        district: "",
+        city: ""
+    },
+    copyright: {
+      title: "",
+        link: "",
+        image: {
+        title: "",
+          link: "",
+          url: "",
+          width: 0,
+          height: 0
+      },
+      provider: [
+        {
+          link: "",
+          name: "",
+          note: ""
+        }
+      ]
+    },
+    Item: {
+      clothes: "",
+        ID: "",
+        url: ""
+    }
     };
-
-    this.handleInput = this.handleInput.bind(this);
-    this.send = this.send.bind(this);
-  }
-
-  handleInput({ target: { value } }) {
-    this.setState({
-      value
-    });
-  }
-
-  send() {
-    const { value } = this.state;
-    this.setState({
-      value: '',
-      message: value
-    });
   }
 
   render() {
-    // const myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "application/json");
-    // const requestOptions = {
-    //   method: 'POST',
-    //   headers: myHeaders,
-    //   body: JSON.stringify({"firstName":"","lastName":""}),
-    //   redirect: 'follow'
-    // }
-    //
-    // fetch("https://viwy3zwne1.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
-    //   .then(response => response.json())
-    //   .then(result => {
-    //     this.setState({jsonObj: JSON.parse(result).body});
-    //   })
-    //   .catch(e => console.log("error", e));
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify({"firstName":"","lastName":""}),
+      redirect: 'follow'
+    }
+
+    fetch("https://viwy3zwne1.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({title: result.title});
+      })
+      .catch(e => console.log("error", e));
 
     return (
         <div>
-          <APITest />
-          <input type="text" value={this.state.value} onChange={this.handleInput} />
-          <button onClick={this.send}>SEND</button>
-          <div>{this.state.message}</div>
+          {/*<APITest />*/}
+          <div>{this.state.title}</div>
         </div>
     );
   }
