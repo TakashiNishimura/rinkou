c5b374cf2cae53dedb427bf73b63389f2afb2382
TakashiNishimura <57384281+TakashiNishimura@users.noreply.github.com>
2021-08-14 15:30:54 +0900

    n

--- src/APITest.js
+++ src/APITest.js
@@ -9,139 +9,149 @@ const APITest = () => {
     title: "",
     link: "",
     description: {
-    //   publicTime: "",
-    //   publicTimeFormatted: "",
-    //   headlineText: "",
-    //   bodyText: "",
-    //   text: ""
+      publicTime: "",
+      publicTimeFormatted: "",
+      headlineText: "",
+      bodyText: "",
+      text: ""
     },
     forecasts: [
-    //   {
-    //     date: "",
-    //     dateLabel: "",
-    //     telop: "",
-    //     detail: {
-    //       weather: "",
-    //       wind: "",
-    //       wave: ""
-    //     },
-    //     temperature: {
-    //       min: {
-    //         celsius: "",
-    //         fahrenheit: ""
-    //       },
-    //       max: {
-    //         celsius: "",
-    //         fahrenheit: ""
-    //       }
-    //     },
-    //     chanceOfRain: {
-    //       T00_06: "",
-    //       T06_12: "",
-    //       T12_18: "",
-    //       T18_24: ""
-    //     },
-    //     image: {
-    //       title: "",
-    //       url: "",
-    //       width: 0,
-    //       height: 0
-    //     }
-    //   },
-    //   {
-    //     date: "",
-    //     dateLabel: "",
-    //     telop: "",
-    //     detail: {
-    //       weather: "",
-    //       wind: "",
-    //       wave: ""
-    //     },
-    //     temperature: {
-    //       min: {
-    //         celsius: "",
-    //         fahrenheit: ""
-    //       },
-    //       max: {
-    //         celsius: "",
-    //         fahrenheit: ""
-    //       }
-    //     },
-    //     chanceOfRain: {
-    //       T00_06: "",
-    //       T06_12: "",
-    //       T12_18: "",
-    //       T18_24: ""
-    //     },
-    //     image: {
-    //       title: "",
-    //       url: "",
-    //       width: 0,
-    //       height: 0
-    //     }
-    //   },
-    //   {
-    //     date: "",
-    //     dateLabel: "",
-    //     telop: "",
-    //     detail: {
-    //       weather: "",
-    //       wind: "",
-    //       wave: ""
-    //     },
-    //     temperature: {
-    //       min: {
-    //         celsius: "",
-    //         fahrenheit: ""
-    //       },
-    //       max: {
-    //         celsius: "",
-    //         fahrenheit: ""
-    //       }
-    //     },
-    //     chanceOfRain: {
-    //       T00_06: "",
-    //       T06_12: "",
-    //       T12_18: "",
-    //       T18_24: ""
-    //     },
-    //     image: {
-    //       title: "",
-    //       url: "",
-    //       width: 0,
-    //       height: 0
-    //     }
-    //   }
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      },
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      },
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      }
     ],
     location: {
-    //   area: "",
-    //   prefecture: "",
-    //   district: "",
-    //   city: ""
+      area: "",
+      prefecture: "",
+      district: "",
+      city: ""
     },
     copyright: {
-    //   title: "",
-    //   link: "",
-    //   image: {
-    //     title: "",
-    //     link: "",
-    //     url: "",
-    //     width: 0,
-    //     height: 0
-    //   },
-    //   provider: [
-    //     {
-    //       link: "",
-    //       name: "",
-    //       note: ""
-    //     }
-    //   ]
+      title: "",
+      link: "",
+      image: {
+        title: "",
+        link: "",
+        url: "",
+        width: 0,
+        height: 0
+      },
+      provider: [
+        {
+          link: "",
+          name: "",
+          note: ""
+        }
+      ]
+    },
+    Item: {
+      clothes: "",
+      ID: "",
+      url: ""
     }
   });
 
   useEffect(() => {
     // fetch("https://weather.tsukumijima.net/api/forecast/city/400040")
-    fetch("https://viwy3zwne1.execute-api.us-east-1.amazonaws.com/dev", {method:"POST"})
+    fetch("https://viwy3zwne1.execute-api.us-east-1.amazonaws.com/dev", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({"firstName":"","lastName":""}),
+        redirect: 'follow'
+      })
       .then(response => response.json())
       .then(data => {
         console.log(data);
@@ -152,6 +162,7 @@ const APITest = () => {
   return (
     <div>
       <a href={posts.link}>{posts["description"]["publicTime"]}</a>
+      <img src={posts["Item"]["url"]}/>
     </div>
   );
 }
