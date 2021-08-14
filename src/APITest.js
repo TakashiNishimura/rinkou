e3395d0d987167552c964c87c3a8b4b660593c86
TakashiNishimura <57384281+TakashiNishimura@users.noreply.github.com>
2021-08-14 14:52:57 +0900

    j

--- src/APITest.js
+++ src/APITest.js
@@ -2,27 +2,155 @@ import React, {useState, useEffect} from "react";
 
 const APITest = () => {
 
-  const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState({
+    publicTime: "",
+    publicTimeFormatted: "",
+    publishingOffice: "",
+    title: "",
+    link: "",
+    description: {
+      publicTime: "",
+      publicTimeFormatted: "",
+      headlineText: "",
+      bodyText: "",
+      text: ""
+    },
+    forecasts: [
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      },
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      },
+      {
+        date: "",
+        dateLabel: "",
+        telop: "",
+        detail: {
+          weather: "",
+          wind: "",
+          wave: ""
+        },
+        temperature: {
+          min: {
+            celsius: "",
+            fahrenheit: ""
+          },
+          max: {
+            celsius: "",
+            fahrenheit: ""
+          }
+        },
+        chanceOfRain: {
+          T00_06: "",
+          T06_12: "",
+          T12_18: "",
+          T18_24: ""
+        },
+        image: {
+          title: "",
+          url: "",
+          width: 0,
+          height: 0
+        }
+      }
+    ],
+    location: {
+      area: "",
+      prefecture: "",
+      district: "",
+      city: ""
+    },
+    copyright: {
+      title: "",
+      link: "",
+      image: {
+        title: "",
+        link: "",
+        url: "",
+        width: 0,
+        height: 0
+      },
+      provider: [
+        {
+          link: "",
+          name: "",
+          note: ""
+        }
+      ]
+    }
+  });
 
   useEffect(() => {
     fetch("https://weather.tsukumijima.net/api/forecast/city/400040")
       .then(response => response.json())
-      .then(data => setPosts(data))
+      .then(data => {
+        console.log(data);
+        setPosts(data);
+      })
   },[]);
-
-  console.log(posts)
 
   return (
     <div>
-      <ul>
-      {
-        posts.map(item => (
-          <li key={item.objectId}>
-            <a href={item.link}>{item.title}</a>
-          </li>
-        ))
-      }
-      </ul>
+      <a href={posts.link}>{posts.title}</a>
     </div>
   );
 }
